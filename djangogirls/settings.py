00f943564d448b735c7d70d69820e3cf4b03d440
piko <kenkonatsu@yahoo.co.jp>
2020-09-11 11:28:20 +0900

    Modified templates to display posts from database.

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z86^f(qef9j3b2+h_alt=9&b#ti($62=(68s+evpxao%&n3p_y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
