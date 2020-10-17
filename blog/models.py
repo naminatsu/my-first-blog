6acfae861009ddb792462c6c47e70fb666412a10
naminatsu <kenkonatsu@yahoo.co.jp>
2020-10-17 18:20:09 +0900

    Added views to image post inside the site.

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,11 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model): # オブジェクト
+class Post(models.Model): # ブログ
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # 他のモデルへのリンク
     title = models.CharField(max_length=200)
     text = models.TextField()
+    picture = models.ImageField(upload_to='images/',default='defo')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -15,5 +16,31 @@ class Post(models.Model): # オブジェクト
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title # タイトルを返す
+
+class Comment(models.Model): # コメント
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+"""
+class Image(models.Model):
+    picture = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+"""
--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -18,9 +18,12 @@ from django.urls import path, include
 
 from django.contrib.auth import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls), # */adminの時，admin.site.urlsへ
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('', include('blog.urls')), # /の時，blog.urlsへ
-]
+    path('', include('blog.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
